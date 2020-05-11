017c3d77189c4493a14767d4432b3e3a1a6b8f1b
Colin Pistell <colin.pistell@looker.com>
2020-05-10 21:23:36 -0600

    massive refactor to add non-unique space names and recursion

--- deploy_content.py
+++ deploy_content.py
@@ -1,4 +1,3 @@
-import networkx as nx
 import os
 import re
 import subprocess
@@ -17,10 +16,10 @@ def get_client(ini, env):
     return sdk
 
 
-def get_space_ids_from_name(space_name, sdk):
-    if space_name == "Shared":
-        return [1]
-    space_list = sdk.search_spaces(name=space_name)
+def get_space_ids_from_name(space_name, parent_id, sdk):
+    if (space_name == "Shared" and parent_id == "0"):
+        return ["1"]
+    space_list = sdk.search_spaces(name=space_name, parent_id=parent_id)
     id_list = [i.id for i in space_list]
 
     return id_list
@@ -29,12 +28,12 @@ def get_space_ids_from_name(space_name, sdk):
 def create_or_return_space(space_name, parent_id, sdk):
 
     try:
-        target_id = get_space_ids_from_name(space_name, sdk)
+        target_id = get_space_ids_from_name(space_name, parent_id, sdk)
         logger.debug("Space ID from name", extra={"id": target_id})
         assert len(target_id) == 1
     except AssertionError as e:
         if len(target_id) > 1:
-            logger.error("More than one Space found with that name", extra={"space_ids": target_id})
+            logger.error("More than one Space found with that parent/name", extra={"space_ids": target_id})
             raise e
         else:
             logger.warning("No spaces found. Creating space now")
@@ -46,51 +45,12 @@ def create_or_return_space(space_name, parent_id, sdk):
     return target_id[0]
 
 
-def build_directory_graph(root_dir):
-    dg = nx.DiGraph()
-    for p, d, f in os.walk(root_dir):
-        cleaned = p.rstrip(os.path.sep)
-        endpoint = cleaned.rpartition(os.path.sep)[2]
-
-        space_file = [i for i in f if re.search("^Space", i)][0]
-        look_files = [i for i in f if re.search("^Look", i)]
-        dash_files = [i for i in f if re.search("^Dashboard", i)]
-
-        logger.debug("graph elements", extra={
-            "endpoint": endpoint,
-            "dirs": d,
-            "space_file": space_file,
-            "look_files": look_files,
-            "dash_files": dash_files
-        })
-
-        edge_tups = [(endpoint, i) for i in d]
-        logger.debug("edge tuples", extra={"edge_tups": edge_tups})
-        dg.add_edges_from(edge_tups)
-        dg.add_node(endpoint, space=space_file, looks=look_files, dashboards=dash_files, path=p)
-
-    return dg
-
-
 def parse_root(content, root="Shared"):
     a, b, c = content.partition(root)
     root_dir = "".join([a, b])
     logger.debug("Parsed root", extra={"content": content, "root_arg": root, "parsed_root": root_dir})
 
     return root_dir
-
-
-def parse_content_path(path):
-    path = path.rstrip(os.path.sep)
-    first, sep, content_file = path.rpartition(os.path.sep)
-    content_space = first.rpartition(os.path.sep)[2]
-    ref = {
-        "content_space": content_space,
-        "content_file": content_file
-    }
-
-    logger.debug("Parsed content path", extra={"ref": ref})
-    return ref
 
 
 def get_gzr_creds(ini, env):
@@ -153,63 +113,93 @@ def import_content(content_type, content_json, space_id, env, ini):
     ])
 
 
-def deploy_space(space, dg, sdk):
-    if space == "Shared":
-        space_id = "1"
-    else:
-        space_parent = get_space_ids_from_name(list(dg.predecessors(space))[0], sdk)[0]
+def build_spaces(spaces):
+    # seeding initial value of parent id to Shared
+    id_tracker = ["0"]
+
+    for space in spaces:
+        logger.debug("parent_id to use", extra={"id_tracker": id_tracker})
+        space_parent = id_tracker.pop()
+
         logger.debug("data for space creation", extra={"space": space, "space_parent": space_parent})
         space_id = create_or_return_space(space, space_parent, sdk)
 
-    return space_id
+        # Add new id to parent tracker
+        id_tracker.append(space_id)
+        logger.debug("parent_id_tracker updated", extra={"parent_id_tracker": id_tracker})
+
+    return id_tracker[0]
 
 
-def deploy_content(content_type, content_list, dg, root_dir, sdk, env, ini):
+def deploy_space(spaces, sdk, env, ini, recursive):
+
+    logger.debug("Deploying spaces", extra={"spaces": spaces})
+    for s in spaces:
+
+        logger.debug("working space", extra={"working_space": s})
+
+        # grab the relevant files for deployment
+        space_files = [f for f in os.listdir(s) if os.path.isfile(os.path.join(s, f))]
+        space_children = [os.path.join(s, d) + "/" for d in os.listdir(s) if os.path.isdir(os.path.join(s, d))]
+        look_files = [os.path.join(s, i) for i in space_files if re.search("^Look", i)]
+        dash_files = [os.path.join(s, i) for i in space_files if re.search("^Dashboard", i)]
+        logger.debug("files to process", extra={"looks": look_files, "dashboards": dash_files})
+
+        # cut down directory to looker-specific paths
+        a, b, c = s.partition("Shared")  # Hard coded to Shared for now TODO: Change this!
+        logger.debug("space components", extra={"a": a, "b": b, "c": c})
+        c = c.rpartition(os.path.sep)[0]
+        logger.debug("new c value", extra={"c": c})
+
+        # turn into a list of spaces to process
+        spaces_to_process = "".join([b, c]).split(os.path.sep)
+        logger.debug("spaces to process", extra={"spaces": spaces_to_process})
+
+        space_id = build_spaces(spaces_to_process)
+        logger.debug("target space id", extra={"space_id": space_id})
+
+        # deploy looks
+        for look in look_files:
+            import_content("look", look, space_id, env, ini)
+        # deploy dashboards
+        for dash in dash_files:
+            import_content("dashboard", dash, space_id, env, ini)
+
+        # go for recursion
+        if recursive and space_children:
+            logger.debug("Attemting Recursion of children spaces", extra={"children_spaces": space_children})
+            for child in space_children:
+                deploy_space([child], sdk, env, ini, recursive)
+        else:
+            logger.debug("No Recursion specified or empty child list", extra={"children_spaces": space_children})
+
+
+def deploy_content(content_type, content_list, sdk, env, ini):
     for c in content_list:
-        parsed = parse_content_path(c)
-        host_space = parsed["content_space"]
-        spaces_to_process = nx.shortest_path(dg, root_dir, host_space)
-        logger.debug("Shortest path found", extra={"spaces_to_process": spaces_to_process})
 
-        for space in spaces_to_process:
-            space_id = deploy_space(space, dg, sdk)
+        # extract directory path
+        dirs = c.rpartition(os.path.sep)[0]
 
-            if space == host_space:
-                # deploy the content
-                import_content(content_type, c, space_id, env, ini)
+        # cut down directory to looker-specific paths
+        a, b, c = dirs.partition("Shared")  # Hard coded to Shared for now TODO: Change this!
+        c = c.rpartition(os.path.sep)[0]  # strip trailing slash
+
+        # turn into a list of spaces to process
+        spaces_to_process = "".join([b, c]).split(os.path.sep)
+
+        space_id = build_spaces(spaces_to_process)
+
+        import_content(content_type, c, space_id, env, ini)
 
 
-def main(root_dir, sdk, env, ini, spaces=None, dashboards=None, looks=None):
-    dg = build_directory_graph(root_dir)
-    root = parse_content_path(root_dir)["content_file"]
-    logger.debug("Parsed root", extra={"root": root})
+def main(sdk, env, ini, spaces=None, dashboards=None, looks=None, recursive=False):
 
     if spaces:
-        logger.debug("Deploying spaces", extra={"spaces": spaces})
-        for s in spaces:
-            parsed_space = parse_content_path(s)["content_file"]
-            logger.debug("Working on space", extra={"space": s, "parsed_space": parsed_space})
-            spaces_to_process = nx.shortest_path(dg, root, parsed_space)
-            logger.debug("Shortest path found", extra={"spaces_to_process": spaces_to_process})
-
-            for space in spaces_to_process:
-                space_id = deploy_space(space, dg, sdk)
-
-                if space == parsed_space:
-                    path = dg.nodes[parsed_space]["path"]
-                    # deploy looks
-                    for look in dg.nodes[parsed_space]["looks"]:
-                        look_path = os.path.join(path, look)
-                        import_content("look", look_path, space_id, env, ini)
-                    # deploy dashboards
-                    for dash in dg.nodes[parsed_space]["dashboards"]:
-                        dash_path = os.path.join(path, dash)
-                        import_content("dashboard", dash_path, space_id, env, ini)
-
+        deploy_space(spaces, sdk, env, ini, recursive)
     if dashboards:
-        deploy_content("dashboard", dashboards, dg, root, sdk, env, ini)
+        deploy_content("dashboard", dashboards, sdk, env, ini)
     if looks:
-        deploy_content("look", looks, dg, root, sdk, env, ini)
+        deploy_content("look", looks, sdk, env, ini)
 
 
 if __name__ == "__main__":
@@ -220,6 +210,7 @@ if __name__ == "__main__":
     parser.add_argument("--env", help="What environment to deploy to")
     parser.add_argument("--ini", default=loc, help="ini file to parse for credentials")
     parser.add_argument("--debug", action="store_true", help="set logger to debug for more verbosity")
+    parser.add_argument("--recursive", action="store_true", help="Should spaces deploy recursively")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--spaces", nargs="+", help="Spaces to fully deploy")
     group.add_argument("--dashboards", nargs="+", help="Dashboards to deploy")
@@ -236,9 +227,5 @@ if __name__ == "__main__":
         logger.info("Pulling content from dev", extra={"env": args.env, "pull_location": args.export})
         export_spaces(args.env, args.ini, args.export)
     else:
-
         sdk = get_client(args.ini, args.env)
-        root_finder = (args.spaces or args.dashboards or args.looks)[0]
-        root = parse_root(root_finder)
-
-        main(root, sdk, args.env, args.ini, args.spaces, args.dashboards, args.looks)
+        main(sdk, args.env, args.ini, args.spaces, args.dashboards, args.looks, args.recursive)
