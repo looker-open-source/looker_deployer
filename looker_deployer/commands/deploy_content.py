5a8e1b725e05f5ec02ccde1bdff4cd20dc94bdd7
michaelcramer <michael.cramer@bytecode.io>
2021-07-29 15:32:10 -0700

    update check for target-base

--- looker_deployer/commands/deploy_content.py
+++ looker_deployer/commands/deploy_content.py
@@ -274,8 +274,9 @@ def main(args):
     logger.debug("ini file", extra={"ini": args.ini})
 
     if args.target_folder:
-        # Force any target space override to start from Shared
-        assert args.target_folder.startswith("Shared"), "Target space does not begin with 'Shared', please specify base with --target-base"
+        # Check target-base is set if target-folder does not start with shared
+        if not args.target_folder.startswith("Shared"): #, "Target space does not begin with 'Shared', please specify base with --target-base"
+            assert not args.target_base  == "Shared", "Target folder does not begin with 'Shared', please specify correct base with --target-base"
         # Make sure trailing sep is in place
         if not args.target_folder.endswith(os.sep):
             args.target_folder += os.sep
