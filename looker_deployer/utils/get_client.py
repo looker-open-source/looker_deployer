c9cb353dbf2e9c42485241c2d29f2b2fc8a99d34
AdamMinton <53316110+AdamMinton@users.noreply.github.com>
2022-05-20 09:56:51 -0400

    Custom user agent for Looker Deployer for tracking

--- looker_deployer/utils/get_client.py
+++ looker_deployer/utils/get_client.py
@@ -12,9 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import looker_sdk
+from looker_deployer import version
+from looker_sdk import api_settings, methods, requests_transport,auth_session, serialize, _settings
+from typing import Optional
+
+def configure_sdk(
+    config_file: str = "looker.ini",
+    section: Optional[str] = None,
+    config_settings: Optional[api_settings.ApiSettings] = None,
+) -> methods.Looker31SDK:
+    """Default dependency configuration"""
+    settings = (
+        _settings(config_file, section) if config_settings is None else config_settings
+    )
+    settings.is_configured()
+    settings.headers['User-Agent'] = f"Looker Deployer {version.__version__}"
+    transport = requests_transport.RequestsTransport.configure(settings)    
+    return methods.Looker31SDK(
+        auth_session.AuthSession(settings, transport, serialize.deserialize31, "3.1"),
+        serialize.deserialize31,
+        serialize.serialize31,
+        transport,
+        "3.1",
+    )
 
 
 def get_client(ini, env):
-    sdk = looker_sdk.init31(config_file=ini, section=env)
+    sdk = configure_sdk(config_file=ini, section=env)
     return sdk
